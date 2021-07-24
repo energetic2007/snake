13fc5122f53cb059fde1a64bdbc37462931a5432
Valeriy <valeriy.s.ivanov@noveogroup.com>
2021-07-24 20:43:47 +0300

    Dont spawn list try

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -9,6 +9,8 @@ public class Food : MonoBehaviour
 
     public BoxCollider2D gridArea;
     public int scoreCount = 0;
+    private Vector3 pos;
+
     [SerializeField] private List<BoxCollider2D> _dontSpawn;
 
     // private void FixedUpdate()
@@ -20,15 +22,43 @@ public class Food : MonoBehaviour
         RandomizePosition();
     }
     private void RandomizePosition()
-    {
+    { //Bounds dontSpawnBounds = this._dontSpawn.bounds;
         Bounds bounds = this.gridArea.bounds;
 
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
+        pos.x = Random.Range(bounds.min.x, bounds.max.x);
+        pos.y = Random.Range(bounds.min.y, bounds.max.y);
+
+        for (int i = 0; i < _dontSpawn.Count; i++)
+        {
+            Bounds _dontSpawnBounds = this._dontSpawn[i].bounds;
+            if (_dontSpawn[i].bounds.Contains(pos))
+            {
+                Debug.Log("ping");
+                // pos.x = Random.Range(bounds.min.x, bounds.max.x);
+                // pos.y = Random.Range(bounds.min.y, bounds.max.y);
+                RandomizePosition();
+            }
+
+
+            //float xDontSpawn = _dontSpawn[i].min.x;
+            /* if (x >= _dontSpawnBounds.min.x && x <= _dontSpawnBounds.max.x)
+             {
+                 Debug.Log("ping");
+                 x = Random.Range(bounds.min.x, bounds.max.x);
+             }
+             if (y >= _dontSpawnBounds.min.y && x <= _dontSpawnBounds.max.y)
+             {
+                 Debug.Log("ping");
+                 y = Random.Range(bounds.min.y, bounds.max.y);
+             }
+             */
+            //private List<float> _dontSpawnCoordinate;
+            // float _dontSpawnX = new List<float>() { _dontSpawn[i].min.x, _dontSpawn[i].max.x };
+        }
 
         // float x = 10f;
         //float y = 1f;
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+        this.transform.position = new Vector3(Mathf.Round(pos.x), Mathf.Round(pos.y), 0.0f);
 
         //   Debug.Log("randomizing");
     }
