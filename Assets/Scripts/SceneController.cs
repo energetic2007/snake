39fd19cecdf9bf505f81c12a78aec8a4eaff2309
Anna Pchelintseva <anna.pch.99@gmail.com>
2021-08-13 22:34:10 +0300

    refactoring

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class SceneController : MonoBehaviour
 {
@@ -10,9 +11,8 @@ public class SceneController : MonoBehaviour
     public static int previousSceneIndex;
     public void Select(int numberInBuild)
     {
-        int currentLevel = numberInBuild;
         int levelReached = PlayerPrefs.GetInt("levelReached", 1);
-        if (levelReached < currentLevel) PlayerPrefs.SetInt("levelReached", currentLevel);
+        if (levelReached < numberInBuild) PlayerPrefs.SetInt("levelReached", numberInBuild);
         SceneManager.LoadScene(numberInBuild);
     }
     public void ExitGame()
@@ -23,10 +23,15 @@ public class SceneController : MonoBehaviour
     {
         Time.timeScale = 0;
         InfoPanel.SetActive(true);
+        if(Input.touchCount > 0)
+        {
+            Restime();
+        }
     }
     public void Restime()
     {
         Time.timeScale = 1;
+        InfoPanel.SetActive(false);
     }
     public void LoadPreviousScene()
     {
